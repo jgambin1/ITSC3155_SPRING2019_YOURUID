ccd79d2608db84f76085db885c05dd9724e05cdb
John Gambino <jgambin1@uncc.edu>
2020-01-21 16:19:29 +0000

    RubyBasic - Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,20 @@
+# Lab 1
+# Part I
+def sum arr
+  # YOUR CODE HERE
+  total = 0;
+  for x in arr
+    total+=x;
+  end
+  return total;
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
