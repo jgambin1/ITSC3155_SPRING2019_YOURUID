d9dbb5c3dceca6a33e01b72b4d99fe24f942392e
John Gambino <jgambin1@uncc.edu>
2020-01-22 15:25:45 +0000

    RubyBasics1 - Part 3

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -12,13 +12,24 @@ end
 # Part II
 def max_2_sum arr
   # YOUR CODE HERE
-  total = 0;
-  total = arr.sort.last(2).sum;
-  return total;
+  
+  return arr.sort.last(2).sum;
   
 end
 
 # Part III
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  
+  for i in 0..arr.length - 1
+    j = i + 1
+    for k in j..arr.length - 1
+      sum = arr[i] + arr[k]
+      if sum == n
+        return true
+      end
+    end
+  end
+  return false if arr.length == 1
+  return false
 end
