04f4f41cf89eb31b6026c16154b061ce52c2085b
John Gambino <jgambin1@uncc.edu>
2020-01-27 04:42:43 +0000

    RubyHashes - Part 3

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -34,22 +34,21 @@ end
 # Part III
 def hash_2_array contacts
     # YOUR CODE HERE
-    finalArray = []
-    emailArray = []
-    phoneArray = []
-    nameArray = []
-    
+    finalArr= []
+    emailArr = []
+    phoneArr = []
+    nameArr = []
     
     contacts.each do |key, value|
         key2 = :"#{key}"
+        emailArr << contacts[key2][:email]
+        phoneArr << contacts[key2][:phone]
+        nameArr << "#{key}"
         
-        emailArray << contacts[key2][:email]
-        phoneArray << contacts[key2][:phone]
-        nameArray << "#{key}"
     end
-    finalArray << emailArray
-    finalArray << phoneArray
-    finalArray << nameArray
+    finalArr << emailArr
+    finalArr << phoneArr
+    finalArr << nameArr
     
-    return finalArray
+    return finalArr
 end
