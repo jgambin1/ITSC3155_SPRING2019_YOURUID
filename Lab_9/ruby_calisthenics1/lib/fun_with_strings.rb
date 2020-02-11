7a676a4387fa8b8f83802a134d550d659df0963c
John Gambino <jgambin1@uncc.edu>
2020-02-11 13:27:16 +0000

    Lab 9: Fun With Words – count words

--- Lab_9/ruby_calisthenics1/lib/fun_with_strings.rb
+++ Lab_9/ruby_calisthenics1/lib/fun_with_strings.rb
@@ -3,13 +3,20 @@ module FunWithStrings
     # your code here
     g = self.downcase.gsub(/[^a-z0-9]+/i,'')
     g == g.reverse
-    
   end
+  
   def count_words
     # your code here
+    h = Hash.new(0)
+    g = self.downcase.gsub(/\W+/xi,' ')
+    g.split.each {|x| h[x] = h[x] + 1}
+    h
+  
   end
   def anagram_groups
     # your code here
+    g = self.downcase.split.group_by{ |x| x.chars.sort }.values
+    g
   end
 end
 
