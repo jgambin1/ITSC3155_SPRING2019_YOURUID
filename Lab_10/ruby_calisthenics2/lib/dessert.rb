d4ae6037b8742516e29a1354306ab4fdf5840dea
John Gambino <jgambin1@uncc.edu>
2020-02-16 14:39:11 +0000

    Ruby OOP - Part 1

--- Lab_10/ruby_calisthenics2/lib/dessert.rb
+++ Lab_10/ruby_calisthenics2/lib/dessert.rb
@@ -1,8 +1,33 @@
 class Dessert
   # add code for setters and getters
+  attr_reader :name, :calories
+  attr_writer :name, :calories
+  
   def initialize(name, calories)
+    @name = name
+    @calories = calories
     # your code here
   end
+  
+  # # setter methods
+  # def set_name=(name)
+  #   @name = name
+  # end
+  
+  # def set_calories=(calories)
+  #   @calories = calories
+  # end 
+  
+  # # getter methods
+  # def get_name
+  #   @name
+  # end
+  
+  # def get_calories
+  #   @calories
+  # end
+  
+  
   def healthy?
     # your code here
   end
