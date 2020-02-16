c7fc31e17794408a7ba90c32fa46b8b6da887563
John Gambino <jgambin1@uncc.edu>
2020-02-16 14:43:05 +0000

    Ruby OOP - Part 2

--- Lab_10/ruby_calisthenics2/lib/dessert.rb
+++ Lab_10/ruby_calisthenics2/lib/dessert.rb
@@ -9,31 +9,21 @@ class Dessert
     # your code here
   end
   
-  # # setter methods
-  # def set_name=(name)
-  #   @name = name
-  # end
-  
-  # def set_calories=(calories)
-  #   @calories = calories
-  # end 
-  
-  # # getter methods
-  # def get_name
-  #   @name
-  # end
-  
-  # def get_calories
-  #   @calories
-  # end
-  
-  
   def healthy?
     # your code here
+    if @calories < 200
+      return true
+    else
+      return false
+    end
+    
   end
+  
   def delicious?
     # your code here
+    return true
   end
+  
 end
 
 class JellyBean < Dessert
