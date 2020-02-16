797aecc24f89a63f2566e7da591924f4b147d3ca
John Gambino <jgambin1@uncc.edu>
2020-02-16 15:10:52 +0000

    Ruby 00 Basics - Part 1

--- Lab_11/RubyBasics3/lib/rubybasics3.rb
+++ Lab_11/RubyBasics3/lib/rubybasics3.rb
@@ -1,5 +1,11 @@
 # Ruby Basics Part 3
 
 class BookInStock
-# YOUR CODE HERE
+    # YOUR CODE HERE
+    attr_reader :isbn, :price
+    attr_writer :isbn, :price
+    def initialize(isbn, price)
+        @isbn = isbn
+        @price = price
+    end
 end
