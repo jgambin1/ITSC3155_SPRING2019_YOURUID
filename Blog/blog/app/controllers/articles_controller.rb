6adffeac0c7300388a41ec71c3f01dbafde0c833
John Gambino <jgambin1@uncc.edu>
2020-04-05 23:11:25 +0000

    Added user functionality

--- Blog/blog/app/controllers/articles_controller.rb
+++ Blog/blog/app/controllers/articles_controller.rb
@@ -1,52 +1,53 @@
 class ArticlesController < ApplicationController
-    def new
-        @article = Article.new
-    end
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+    #byebug
     
-    def create
-        
-        @article = Article.new(article_params)
-        #byebug
-        
-        if @article.save 
-            #byebug
-            redirect_to @article 
-        else 
-            render 'new' 
-        end
-
-    end
-    
-    def show
-        @article = Article.find(params[:id])
-    end
-    
-    def index
-        @articles = Article.all
-    end
-    
-    def edit 
-        @article = Article.find(params[:id]) 
-    end
-    
-    def update 
-        @article = Article.find(params[:id]) 
-        if @article.update(article_params) 
-            redirect_to @article 
-        else 
-                render 'edit' 
-        end 
-    end
-    
-    def destroy 
-        @article = Article.find(params[:id]) 
-        @article.destroy
-        redirect_to articles_path 
+    if @article.save 
+      #byebug
+      redirect_to @article
+    else 
+      render 'new'
     end 
-    
-end
+  end 
+   
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def index
+    @articles = Article.all
+  end
 
-private
-    def article_params
-        params.require(:article).permit(:title, :text)
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    respond_to do |format|
+      format.html { redirect_to articles_path }
+    end 
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+end
--- Blog/blog/config/routes.rb
+++ Blog/blog/config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'sessions/new'
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'sessions/create'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'sessions/destroy'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  resources :users
   get 'welcome/index'
   
-  resources :articles do
+  resources :articles do 
     resources :comments
-  end
-  
-  root 'welcome#index'
-
-
+  end 
+  root :to => 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+  
